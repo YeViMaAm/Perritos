583be37f2d2eba79575f69ccc0239e64b668fd59
YeViMaAm <167661055+YeViMaAm@users.noreply.github.com>
2026-02-27 19:29:54 +0000

    Subida inicial Perritos

---
+++ src/exceptions.py
@@ -0,0 +1,26 @@
+class UsuarioNoExiste (Exception):
+    """Se lanza cuando el usuario ingresado no esta registrado"""
+    pass
+
+class ContrasenaIncorrecta (Exception):
+    """Se lanza cuando la contraseña es incorrecta"""
+    pass
+
+class UsuarioVacio(Exception):
+    """Se lanza cuando el usuario ingresado esta vacio"""
+    pass
+
+class ContrasenaVacia (Exception):
+    """Se lanza cuando la contraseña esta vacia"""
+    pass
+
+class UsuarioEspacios (Exception):
+    """Se lanza cuando el usuario ingresado solo contine espacios"""
+    pass
+
+class ContrasenaEspacios (Exception):
+    """Se lanza cuando la contraseña ingresada solo contiene espacios"""
+    pass
+
+
+
---
+++ src/login.py
@@ -0,0 +1,33 @@
+from src.exceptions import  (UsuarioNoExiste, ContrasenaIncorrecta, UsuarioVacio, ContrasenaVacia, UsuarioEspacios, ContrasenaEspacios)
+
+class login:
+
+    def __init__(self, usuario, contrasena):
+        self._validar_usuario(usuario)
+        self._validar_contrasena(contrasena)
+
+        self.usuario = usuario
+        self.contrasena = contrasena
+
+    def _validar_usuario(self, usuario):
+        if not usuario:
+            raise UsuarioVacio("El usuario no pued estar vacio")
+
+        if usuario.strip() == "":
+            raise UsuarioEspacios("El usuario no puede contener solo espacios")
+
+        if usuario != "candy":
+            raise UsuarioNoExiste(f"El usuario no esta registrado: {usuario}")
+
+    def _validar_contrasena(self, contrasena):
+        if not contrasena:
+            raise ContrasenaVacia("La contraseña no puede estar vacía")
+
+        if contrasena.strip() == "":
+            raise ContrasenaEspacios("La contraseña no puede contener solo espacios")
+
+        if contrasena != "candy159":
+            raise ContrasenaIncorrecta(f"La contraseña es incorrecta: {contrasena}")
+
+    def __str__(self):
+        return f"Login(usuario='{self.usuario}')"
