7d0ccc201bb1bee75ed83ee3d6ba9350444e6882
YeViMaAm <167661055+YeViMaAm@users.noreply.github.com>
2026-04-09 18:51:28 +0000

    Se actualiza el archivo login_Controller

--- src/controllers/login_controller.py
+++ src/controllers/login_controller.py
@@ -2,7 +2,7 @@ from src.models.login import Login
 
 def autenticar_usuario(usuario, contrasena):
     try:
-        user = login(usuario, contrasena)
-        return {"success": True, "usuario": user.usuario}
+        login = Login(usuario, contrasena)  # 
+        return {"success": True, "usuario": usuario}
     except Exception as e:
         return {"success": False, "error": str(e)}
