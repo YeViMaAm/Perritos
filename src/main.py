c60bad08f63c9fd2c29d37edf495f0598a2139cd
YeViMaAm <167661055+YeViMaAm@users.noreply.github.com>
2026-04-09 19:42:32 +0000

    Se elimino la carpeta static y se ajusto el main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,9 @@
 
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
 from src.routers import login_router, perrito_router
 
-# Crear la aplicación
 app = FastAPI()
 
-# ✅ Configurar archivos estáticos (CSS)
-app.mount("/static", StaticFiles(directory="src/static"), name="static")
-
-# ✅ Conectar routers
+# Conectar routers
 app.include_router(login_router.router)
 app.include_router(perrito_router.router)
